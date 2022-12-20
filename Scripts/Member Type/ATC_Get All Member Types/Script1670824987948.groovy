6ac9de8b5d539a65cae0ad2df7ae14a4acd8469f
zackyantoleo <zackyanto.leo@gmail.com>
2022-12-21 00:28:49 +0700

    Commit

---
+++ Scripts/Member Type/ATC_Get All Member Types/Script1670824987948.groovy
@@ -0,0 +1,48 @@
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import groovy.json.JsonSlurper as JsonSlurper
+
+response = WS.sendRequest(findTestObject('Login Test/Login Admin'), FailureHandling.CONTINUE_ON_FAILURE)
+
+JsonSlurper slurper = new JsonSlurper()
+
+Map parsedJson = slurper.parseText(response.getResponseText())
+
+String Token = parsedJson.data.access.token
+
+GlobalVariable.AdminToken = Token
+
+response = WS.sendRequest(findTestObject('Login Test/Login User'), FailureHandling.CONTINUE_ON_FAILURE)
+
+JsonSlurper slurper2 = new JsonSlurper()
+
+Map parsedJson2 = slurper2.parseText(response.getResponseText())
+
+String UserToken = parsedJson2.data.access_token
+
+GlobalVariable.UserToken = UserToken
+
+response = WS.sendRequest(findTestObject('Member Type/Get All Member Types/1Positive/Get All Member Types admin'), FailureHandling.CONTINUE_ON_FAILURE)
+
+WS.verifyResponseStatusCode(response, 200, FailureHandling.CONTINUE_ON_FAILURE)
+
+response = WS.sendRequest(findTestObject('Member Type/Get All Member Types/1Positive/Get All Member Types User'), FailureHandling.CONTINUE_ON_FAILURE)
+
+WS.verifyResponseStatusCode(response, 200, FailureHandling.CONTINUE_ON_FAILURE)
+
